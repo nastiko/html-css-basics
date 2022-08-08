9a00d6bc8d9cd8b47a97852074aa4709e2323fe8
Anastasia <anastasia.hrynkevich@outlook.com>
2022-08-08 13:34:25 +0100

    Finding bugs and fixing them. Deleted JS code. In my case better to use a media query.

--- practice/codecademy/capstone/javascript/index.js
+++
@@ -1,8 +0,0 @@
-$(window).on('resize', function() {
-    let windowScreen = $(this);
-    if (windowScreen.width() <= 970) {
-        $('.desktop-device, .img-first_wrap.hide-block, .desktop-img, .hr-first_line, .hr-second_line, .grid-container, .video-subtitle, .video-type_course, .fourth-block_wrap, .footer-list_style').hide();
-    } else {
-        $('.desktop-device, .img-first_wrap.hide-block, .desktop-img, .hr-first_line, .hr-second_line, .grid-container, .video-subtitle, .video-type_course, .fourth-block_wrap, .footer-list_style').show();
-    }
-});
