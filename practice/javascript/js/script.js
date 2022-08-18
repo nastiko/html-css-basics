5601686b6a69f828fbdcbb601897116adfed1e14
Anastasia <anastasia.hrynkevich@outlook.com>
2022-08-18 17:46:26 +0100

    Learning and practising JS. To better understand basic JS, I am repeating the code from scratch.

---
+++ practice/javascript/js/script.js
@@ -0,0 +1,17 @@
+document.getElementById('output').innerHTML = "My First JavaScript";
+
+let num;
+num = 123;
+
+
+let a, b, c;
+a = 5;
+b = 36;
+c = a * b;
+
+let e, d, f;
+e = 10;
+d = 5;
+f = e + d;
+
+
