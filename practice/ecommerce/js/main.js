275b800be3c4630b222cf00cc1ef3e4a8073f39a
Anastasia <anastasia.hrynkevich@outlook.com>
2022-12-08 00:17:03 +0000

    Changed code of click-btn in CSS, worked on moving slide

--- practice/ecommerce/js/main.js
+++ practice/ecommerce/js/main.js
@@ -6,47 +6,26 @@ let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
 //Sliders
 function slider() {
-    let arrows = document.querySelectorAll('.bg-icon');
-    for(let i = 0; i < arrows.length; i++) {
-        arrows[i].addEventListener('click', nextSlide);
+    let sliders = document.querySelectorAll('.carousel_slide');
+    let nextBtn = document.getElementById('right-click');
+    let prevBtn = document.getElementById('left-click');
+    let indicatorNav = document.querySelectorAll('.carousel_nav button');
+    let currentPosition = 0;
+    let viewportWidth = window.innerWidth;
+    //console.log(viewportWidth);
+
+
+    for(let i = 0; i < sliders.length; i++) {
+        sliders[i].style.left = viewportWidth * i + 'px';
     }
 
-    //устанавливаем индекс слайда по умолчанию
-    let slideIndex = 1;
-    showSlides(slideIndex);
-
-    function nextSlide() {
-        //увеличиваем индекс на 1, то есть показываем следующий слайд
-        showSlides(slideIndex += 1);
+    nextBtn.addEventListener('click', moveRight);
+    function moveRight() {
+        let currentSlide = document.getElementById('current-slide');
+        let nextSlide = currentSlide.nextElementSibling;
+        let amountToMove = nextSlide.style.left;
+        sliders.style.transform = 'translateX(' + amountToMove + ')';
     }
-
-    function previousSlide() {
-        //уменьшаем индекс на 1, то есть показываем предыдущий слайд
-        showSlides(slideIndex -= 1);
-    }
-
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-        let sliders = document.getElementsByClassName('slider');
-
-        if (n > sliders.length) {
-            slideIndex = 1;
-        }
-        if (n < 1) {
-            slideIndex = sliders.length;
-        }
-
-        for (let slide of sliders) {
-            slide.style.display = 'none';
-        }
-        sliders[slideIndex - 1].style.display = 'block';
-    }
-
-
-    //let widthWindow = window.innerWidth;
 }
 
 slider();
