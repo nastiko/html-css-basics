5a8af80c1fc6cf8bd04c68291c0e49c7e2490546
Anastasia <anastasia.hrynkevich@outlook.com>
2023-07-02 01:26:34 +0100

    - completed to validate discount form

--- practice/ecommerce/js/checkout.js
+++ practice/ecommerce/js/checkout.js
@@ -3,13 +3,18 @@ class ValidationForm {
     #email;
     #firstName;
 
+    #setDiscount = 'sale-25';
+    #discount;
+    #applyDiscountBtn;
 
     #validationResult = true;
 
-    constructor(form, email = '#email', firstName = '#first-name') {
+    constructor(form, email = '#email', firstName = '#first-name', discount = 'discount', applyDiscountBtn = 'apply-discount') {
         this.#form = document.getElementById(form);
         this.#email = this.#form.querySelector(email);
         this.#firstName = this.#form.querySelector(firstName);
+        this.#discount = document.getElementById(discount);
+        this.#applyDiscountBtn = document.getElementById(applyDiscountBtn);
     }
 
     getEmailValue() {
@@ -31,11 +36,11 @@ class ValidationForm {
         this.#validationResult = false;
     }
 
-    setSuccess(elem) {
+    setSuccess(elem, message) {
         let inputControl = elem.parentElement;
         let errorDisplay = inputControl.querySelector('.error');
 
-        errorDisplay.innerHTML = '';
+        errorDisplay.innerHTML = message;
         inputControl.classList.add('success');
         inputControl.classList.remove('error');
     }
@@ -51,7 +56,7 @@ class ValidationForm {
             this.setError(this.#email, 'Email is required');
         } else if(!this.isValidEmail(this.getEmailValue())) {
             this.setError(this.#email, 'Provide a valid email address');
-        } this.setSuccess(this.#email);
+        } this.setSuccess(this.#email, '');
 
         // First Name
         if(this.getFirstNameValue() === '') {
@@ -64,7 +69,23 @@ class ValidationForm {
 
     }
 
+    applyDiscount(event) {
+        let userValue = this.#discount.value;
+        if(userValue === this.#setDiscount) {
+            this.setSuccess(this.#discount, 'Discount is successful!');
+        } else {
+            this.setError(this.#discount, 'Discount is not successful!');
+        }
+
+        event.preventDefault();
+    }
+
+    init() {
+        this.#applyDiscountBtn.addEventListener('click', (event) => this.applyDiscount(event));
+    }
+
 }
 
 let validation = new ValidationForm('form-checkout');
 validation.validationInput();
+validation.init();
